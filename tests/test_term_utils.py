55732a434d0789bc58a1549a4ca9e6d343cf29a1
Sandeep Ghael <sghael@gmail.com>
2024-06-24 14:41:53 -0400

    simplify the test case

--- tests/test_term_utils.py
+++ tests/test_term_utils.py
@@ -44,15 +44,16 @@ def test_parse_with_escape_blocks():
             'this is a prompt  {start}my first context block{end} and then ```my second context block```',
             {'bar': '1.0', 'baz': '2.0'},
         ),
+        (
+            "{start}this is a prompt --bar=1.0 --baz=2.0{end}",
+            "{start}this is a prompt --bar=1.0 --baz=2.0{end}",
+            {}
+        )
     ]
 
     delimiters = ["```", '"""', "`"]
 
     for start, end in [(d, d) for d in delimiters]:
-        assert parse_args(f"{start}this is a prompt --bar=1.0 --baz=2.0{end}") == (
-            f"{start}this is a prompt --bar=1.0 --baz=2.0{end}",
-            {},
-        )
         for prompt, expected_prompt, expected_args in test_cases:
             formatted_prompt = prompt.format(start=start, end=end)
             formatted_expected_prompt = expected_prompt.format(start=start, end=end)
