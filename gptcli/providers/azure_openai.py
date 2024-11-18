1b4cc141a849e04125002f899b4ba27ffa4a9afd
trojan-bumble-bee <trojan.bumble.bee@gmail.com>
2024-11-18 22:18:22 +0000

    add super().__init__()

--- gptcli/providers/azure_openai.py
+++ gptcli/providers/azure_openai.py
@@ -5,6 +5,7 @@ from gptcli.providers.openai import OpenAICompletionProvider
 
 class AzureOpenAICompletionProvider(OpenAICompletionProvider):
     def __init__(self):
+        super().__init__()
         self.client = AzureOpenAI(
             api_key=openai.api_key,
             base_url=openai.base_url,
